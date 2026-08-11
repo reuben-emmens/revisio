a5f0dc993ae48b52ccb03190556d07a7aa0d2c4f
Reuben Emmens <reuben@emmens.uk>
2026-08-11 17:24:44 +0100

    build: install Just in dev-tools.sh convenience script

--- hack/dev-tools.sh
+++ hack/dev-tools.sh
@@ -1,1 +1,7 @@
+# Just
+mkdir -p ~/bin
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/bin
+export PATH="$PATH:$HOME/bin"
+
+# Golangci-lint
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.11.4
